74ecc8f3c9810b039456be8e3046d63df4f81b6d
Mykhailo <mykhailotiutiun@gmail.com>
2022-06-22 12:45:15 +0300

    Create Java Project with Maven and Spring

---
+++ src/main/java/com/mykhailotiutiun_projects/onlinediary/OnlineDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mykhailotiutiun_projects.onlinediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mykhailotiutiun_projects/onlinediary/OnlineDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykhailotiutiun_projects.onlinediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
